411657787a17819fa278c7e46b4c5e5bb50f93fb
Maksim Matveev <thematveev21@gmail.com>
2024-07-26 02:34:33 +0300

    countdown

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,16 +1,20 @@
 const ldr = document.querySelector('.loader');
 const vid = document.getElementById('vid');
 
-var dcl = new Promise(function(resolve) {
-    document.addEventListener("DOMContentLoaded",resolve,false);
-})
-var deviceready = new Promise(function(resolve) {
-    vid.addEventListener("loadeddata", resolve, false);
-})
+// var dcl = new Promise(function(resolve) {
+//     document.addEventListener("DOMContentLoaded",resolve,false);
+// })
+// var deviceready = new Promise(function(resolve) {
+//     vid.addEventListener("loadeddata", resolve, false);
+// })
 
-Promise.all([dcl, deviceready]).then(function() {
+// Promise.all([dcl, deviceready]).then(function() {
+//     ldr.remove();
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
     ldr.remove();
-});
+})
 
 // Promise.all([dcl]).then(function() {
 //     ldr.remove();
